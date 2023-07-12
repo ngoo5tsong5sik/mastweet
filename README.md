293c3f3ae97e2ac647db3b40c47162602926bf9f
gcourtade <47104451+gcourtade@users.noreply.github.com>
2023-07-13 00:35:18 +0200

    Using OAuth1a

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Mastweet
 ---
-This script syncronizes Mastodon toots to Twitter.
-It runs as a cron GitHub Action every 10 min to sync your latest Toot to Twitter. This  was adapted from https://github.com/klausi/mastodon-twitter-sync/
+This script cross-posts Mastodon toots to Twitter. The code was adapted from https://github.com/twitterdev/FactualCat-Twitter-Bot
+
+It runs as a cron GitHub Action every 10 min to post your latest Toot to Twitter. This action was adapted from https://github.com/klausi/mastodon-twitter-sync/
 
 To use it, you need a Twitter Developer account (I am using the Free plan).
 1. Create a new Github environment with the name "Cron" at `https://github.com/<USERNAME>/mastweet/settings/environments/new`
-2. For access to Mastodon add the `CLIENT_ID`, `CLIENT_SECRET`, `ACCESS_TOKEN` secrets to the Cron environment.
-3. For access to Twitter add the `BEARER_TOKEN` secret to the Cron environment.
-4. Change the `api_base_url= 'https://mstdn.social'` variable to the URL of your Mastodon instance.
+2. For this to work with your accounts, add the corresponding values to the `TWITTER_CONSUMER_KEY`, `TWITTER_CONSUMER_SECRET`,`TWITTER_ACCESS_TOKEN`,`TWITTER_ACCESS_TOKEN_SECRET`,`MASTODON_CLIENT_ID`, `MASTODON_CLIENT_SECRET`, `MASTODON_ACCESS_TOKEN` and `MASTODON_INSTANCE_URL` (e.g. https://mstdn.social) secrets to the Cron environment.
+
+
 
--- mastweet.py
+++ mastweet.py
@@ -1,16 +1,16 @@
-import tweepy
 from mastodon import Mastodon
 from bs4 import BeautifulSoup
+from requests_oauthlib import OAuth1
 import requests
 import pickle
 import os
 
 # Mastodon API setup
 mastodon = Mastodon(
-    client_id=os.environ.get("CLIENT_ID"),
-    client_secret = os.environ.get("CLIENT_SECRET"),
-    access_token = os.environ.get("ACCESS_TOKEN"),
-    api_base_url= 'https://mstdn.social'
+    client_id=os.environ.get("MASTODON_CLIENT_ID"),
+    client_secret = os.environ.get("MASTODON_CLIENT_SECRET"),
+    access_token = os.environ.get("MASTODON_ACCESS_TOKEN"),
+    api_base_url= os.environ.get("MASTODON_INSTANCE_URL"),
 )
 user = mastodon.account_verify_credentials()
 user_id = user['id']
@@ -37,41 +37,38 @@ if latest_toot_id not in synced_toots:
     soup = BeautifulSoup(latest_toot_content, 'html.parser')
     latest_toot_text = soup.get_text()
 
-    # Create payload
-    payload = {
-            "text": latest_toot_text
-    }
-    # Get new token
+    # Twitter API setup
+    consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-    get_token = requests.request(
-        "POST",
-        "https://api.twitter.com/2/oauth2/token",
-        headers={
-            'Content-Type':'application/x-www-form-urlencoded'
-        },
-        data={
-            'refresh_token':os.environ.get('TWITTER_REFRESH_TOKEN'),
-            'grant_type':'refresh_token',
-            'client_id':os.environ.get('TWITTER_CLIENT_ID')
-        }
-    )
+    def connect_to_oauth(consumer_key, consumer_secret, acccess_token, access_token_secret):
+        url = "https://api.twitter.com/2/tweets"
+        auth = OAuth1(consumer_key, consumer_secret, acccess_token, access_token_secret)
+        return url, auth
 
-    # Post tweet
-    response = requests.request(
-        "POST",
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-        headers={
-            "Authorization": "Bearer {}".format(get_token),
-            "Content-Type": "application/json",
-        },
-    )
 
-    # Check response
-    if response.status_code != 201:
-        raise Exception(f"Request returned error: {response.status_code}, {response.text}")
-    else:
-        print("Successfully tweeted!")
+    def main():
+        payload = { "text": latest_toot_text }
+
+        url, auth = connect_to_oauth(
+            consumer_key, consumer_secret, access_token, access_token_secret
+        )
+        print(url, auth)
+        request = requests.post(
+            auth=auth, url=url, json=payload, headers={"Content-Type": "application/json"}
+        )
+
+        # Check response
+        if request.status_code != 201:
+            raise Exception(f"Request returned error: {request.status_code}, {request.text}")
+        else:
+            print("Successfully tweeted!")
+
+    main()
+
+
 else:
     print('Toot already synced')
 
